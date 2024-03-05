6d3b5bd19e0cf35de00afd25903f63a681bcebc6
Kauan Perdigão <84131424+Kauan678@users.noreply.github.com>
2024-03-04 22:53:46 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+//import HelloWorld from "./components/01HelloWorld/HelloWorld";
+//import FuncoesInternas from "./components/02FuncoesInternas/FuncoesInternas";
+//import Loja from "./components/03Props/Loja";
+//import MeusDados from "./components/01MeusDados";
+//import MeusDados from "./components/02MeusDados";
+import Temperatura from "./components/03temperatura";
+
+function App() {
+  return (
+    <div>
+      <Temperatura c = {40} f = {40} k = {40}/>
+    </div>
+  )
+}
+export default App

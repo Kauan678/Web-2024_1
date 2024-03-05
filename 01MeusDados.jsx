ac7adf357db9641409f65af1ad9d4d252b87cc6d
Kauan Perdigão <84131424+Kauan678@users.noreply.github.com>
2024-03-04 22:14:40 -0300

    Add files via upload
    Este repositório é dedicado a dsciplina de Desenvolvimento de Software para Web!

---
+++ 01MeusDados.jsx
@@ -0,0 +1,11 @@
+
+const MeusDados = () => {
+    return (
+      <div>
+      <h1>Nome: Kauan Oliveira Perdigão Lopes</h1>
+      <h1>Curso: Sistemas de Informação</h1>
+      <h1>Universidade: Universidade Federal do Ceará</h1>
+      </div>
+    )
+}
+export default MeusDados
